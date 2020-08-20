414ff595976e3f7e3cfee574f408e0feedbafa6e
santoshakil <shakil.mezbah@gmail.com>
2020-08-20 17:32:13 +0600

    1st

--- lib/app.dart
+++ lib/app.dart
@@ -6,14 +6,11 @@ import 'product_list_tab.dart';
 import 'search_tab.dart';
 import 'shopping_cart_tab.dart';
 
-
 class CupertinoStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]
-    );
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
     return CupertinoApp(
       home: CupertinoStoreHomePage(),
@@ -24,46 +21,55 @@ class CupertinoStoreApp extends StatelessWidget {
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.home),
-            title: Text('Products'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.search),
-            title: Text('Search'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.shopping_cart),
-            title: Text('Cart'),
-          )
-        ]
-        
-      ),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.home),
+          // ignore: deprecated_member_use
+          title: Text('Products'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.search),
+          // ignore: deprecated_member_use
+          title: Text('Search'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.shopping_cart),
+          // ignore: deprecated_member_use
+          title: Text('Cart'),
+        )
+      ]),
       tabBuilder: (context, index) {
         CupertinoTabView returnValue;
         switch (index) {
-          case 0 : 
-            returnValue = CupertinoTabView(builder: (context) {
-              return CupertinoPageScaffold(child: ProductListTab());
-            },);
+          case 0:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return CupertinoPageScaffold(child: ProductListTab());
+              },
+            );
             break;
           case 1:
-            returnValue = CupertinoTabView(builder: (context) {
-              return CupertinoPageScaffold(child: SearchTab(),);
-            },);
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return CupertinoPageScaffold(
+                  child: SearchTab(),
+                );
+              },
+            );
             break;
           case 2:
-            returnValue = CupertinoTabView(builder: (context) {
-              return CupertinoPageScaffold(child: ShoppingCartTab(),);
-            },);
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return CupertinoPageScaffold(
+                  child: ShoppingCartTab(),
+                );
+              },
+            );
             break;
         }
         return returnValue;
       },
-
     );
   }
 }
